3e2c2159ad56e1202f7d366db7f5647fbb5af6be
syarifah febriyani <syarifahfebyani@gmail.com>
2026-03-13 09:18:23 +0700

    perbaikan PEP 8: Menambahkan docstring

--- code_set_3.py
+++ code_set_3.py
@@ -1,8 +1,33 @@
+"""
+Nama File : code_set_3.py
+Tujuan Program : Menghitung nilai rata-rata dari sekumpulan data angka
+Fitur utama : Menghitung rata-rata dengan membagi jumlah total dengen 
+banyak data
+Identitas Penulis : Syarifah Febriyani
+Tanggal Pembaruan Terakhir : 13 maret 2026
+
+"""
 def avg(data):
+    """
+    Parameter : 
+    data : list yang berisi sekumpulan data yang akan dihitung
+    rata-ratanya
+
+    Tipe Data :
+    integer dan float
+
+    Nilai Kembalian :
+    Float (bilangan pecahan)
+
+    Contoh Pemanggilan :
+    avg ([5, 10, 15, 20])
+    12.5
+
+    """
     total = 0
     for d in data:
         total += d
     return total/len(data)
 
 
-print(avg([]))
+print(avg([5, 10, 15, 20]))
